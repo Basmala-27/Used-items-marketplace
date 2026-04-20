927ef338b05eea91f304819db6be84baa8711cad
Basmala Mohamed <Basmala01067618897@gmail.com>
2026-04-20 04:25:17 +0200

    Add: basics asp.net core files

---
+++ items marketplace/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using items_marketplace.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace items_marketplace.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
