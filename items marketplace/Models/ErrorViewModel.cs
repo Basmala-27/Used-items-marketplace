9d9f5ca5f4d1a2eaf4d2bf985e88a81dbeb43289
Tasneem744 <wwww95287@gmail.com>
2026-04-24 12:01:44 +0300

    Permanently deleted the old items marketplace folder

--- items marketplace/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace items_marketplace.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
