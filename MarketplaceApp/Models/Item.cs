6ec5ad24afafb43a178712b9e3a6e0250a42ea98
arwa khalid <arwa.rorokh25@gmail.com>
2026-04-25 16:55:13 +0300

    add item and itemimages models

---
+++ MarketplaceApp/Models/Item.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Item
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ItemID { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(200, ErrorMessage = "Title can't exceed 200 characters")]
+    public string Title { get; set; }
+
+    [StringLength(1000)]
+    [DataType(DataType.MultilineText)]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Price is required")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Range(0.01, 100000000, ErrorMessage = "Price must be greater than 0")]
+    public decimal Price { get; set; }
+
+    [StringLength(50)]
+    public string Condition { get; set; }
+
+    [StringLength(100)]
+    public string Location { get; set; }
+
+    [StringLength(50)]
+    public string Status { get; set; }
+
+    // FK
+    [Required]
+    public int UserID { get; set; }
+
+    [Required]
+    public int CategoryID { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    // Navigation Properties
+    [ForeignKey(nameof(UserID))]
+    public User User { get; set; }
+
+    [ForeignKey(nameof(CategoryID))]
+    public Category Category { get; set; }
+
+    // Relationships
+    public ICollection<ItemImage> Images { get; set; } = new List<ItemImage>();
+}
---
+++ MarketplaceApp/Models/ItemImage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ItemImage
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ImageID { get; set; }
+
+    [Required(ErrorMessage = "Image URL is required")]
+    [StringLength(500, ErrorMessage = "Image URL can't exceed 500 characters")]
+    [DataType(DataType.ImageUrl)]
+    public string ImageUrl { get; set; }
+
+    // FK
+    [Required]
+    public int ItemID { get; set; }
+
+    [ForeignKey(nameof(ItemID))]
+    public Item Item { get; set; }
+}
